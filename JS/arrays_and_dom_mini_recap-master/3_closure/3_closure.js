5d48ac63db2db749a2997dc072306021ecdfde17
mbukhman <mbukhman@gmail.com>
2023-02-19 13:32:13 +0400

    update

---
+++ JS/arrays_and_dom_mini_recap-master/3_closure/3_closure.js
@@ -0,0 +1,44 @@
+// Example
+// Create a function makePassword that returns a function
+// that takes in a password and returns a message indicating whether the input matches the original password.
+
+function makePassword(password) {
+    return function (input) {
+        return input === password ? "Access granted." : "Access denied.";
+    };
+}
+
+const checkPassword = makePassword("secret");
+// console.log(checkPassword("secret")); // "Access granted."
+// console.log(checkPassword("notsecret")); // "Access denied."
+
+const checkPassword2 = makePassword("secret2");
+// console.log(checkPassword2("secret2")); // "Access granted."
+// console.log(checkPassword2("notsecret2")); // "Access denied."
+
+// Explanation: The function makePassword returns another function
+// that takes in an input and compares it to the original password.
+// The inner function has access to the password variable, creating a closure.
+
+// 1. Create a function counter that returns a function that increments and returns a counter variable.
+// The counter should not be accessible from outside the function.
+
+function createNewIdGenerator() {
+    let i = 0;
+    return () => {
+        i += 1;
+        console.log(i);
+    };
+}
+genId = createNewIdGenerator();
+genId();
+genId();
+genId();
+genId();
+genId();
+
+// 2. Create a function addMaker that takes in a value and returns a function that adds that value to its input.
+
+// 3. Create a function adder that takes in a value and returns an object with two methods: add and reset.
+// The add method takes in a number and returns the sum of the original value and the input,
+// and the reset method resets the value back to the original value.
--- JS/arrays_and_dom_mini_recap-master/5_event_listeners/script.js
+++ JS/arrays_and_dom_mini_recap-master/5_event_listeners/script.js
@@ -1,13 +1,21 @@
 // Exercise 1: Add a click event listener to the first box element
 // that logs "First box clicked" to the console when clicked
 // Make the callback function that is being passed in to event listener a named function
+let x = 10;
+x++;
+const firstBox = document.querySelector("#container >:first-child ");
+firstBox.addEventListener("click", (e) => {
+    event.stopPropagation();
 
+    console.log("First box clicked");
+});
 
 // Exercise 2: Add a click event listener to the container element
 // that logs "Container clicked" to the console when clicked
 // Click on the first box and see which event listener is called first
 // Make the callback functions that are being passed in to event listeners named function
-
+const container = document.querySelector("#container");
+container.addEventListener("click", (e) => console.log("Container clicked"));
 
 // Exercise 3: Add the following code inside the first box event listener function:
 // event.stopPropagation();
@@ -15,13 +23,16 @@
 // Make the callback function that is being passed in to event listener of the Container a named function
 // and the function that is being passed in to the event listener of the first box an anonyms function
 
-
 // Exercise 4: Remove the event.stopPropagation() method from the first box element
 // Make the event listener run only once
-
-
+{ once: true }
+or
+button.addEventListener('click', function handler() {
+    ///this will execute only once
+    alert('only once!');
+    this.removeEventListener('click', handler);
+});
 // Exercise 5: Use the removeEventListener method to remove the event listener from the first box element
-
 
 // Exercise 6: Add a click event listener to the container element that logs the background-color
 // of the clicked box to the console
